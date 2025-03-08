502701f35249360b3faf40ebf1f845ac280b9a6f
Tiko Tkabladze <tikotyablaze@gmail.com>
2025-03-08 22:13:11 +0400

    My practice for rstrip ,lstrip,and strip

--- special_charecters.py
+++ special_charecters.py
@@ -1,4 +1,4 @@
-print('Languages:\n\tPython\nC\n\tJavaScript')
+#print('Languages:\n\tPython\nC\n\tJavaScript')
 
 
 favourite_language = 'python ' 
@@ -7,5 +7,9 @@ favourite_language = " python"
 ll = favourite_language.lstrip()
 favourite_language = ' python '
 var = favourite_language.strip()
-print(rl)
+name = ' tiko tyabladze '
+r_name = name.rstrip()
+l_name = name.lstrip()
+s_name = name.strip()
+print(f'-{r_name}-{l_name}-{s_name}-')
 pass
