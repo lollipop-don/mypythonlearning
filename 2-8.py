b4d21fb4c7a0e18c82cd0405ddc31fe20693a526
Tiko Tkabladze <tikotyablaze@gmail.com>
2025-03-11 00:20:38 +0400

    Double quotes and single quotes intermixed

---
+++ 2-8.py
@@ -0,0 +1,14 @@
+#notes = 'python_notes.txt'
+#print(notes.removesuffix('.txt'))
+#name = 'strange.string'
+#print(name.removeprefix('strange.'))
+#quot = "Albert Einstein once said, \"A person who never made a mistake never tried anything new.\""
+#print(quot)
+#language = "bcfgcfxgfx: \"dsfgh,gmncbxvxxcgn\" hjkhmgnfbfgdghjkhjhmnb"
+#print(language)
+#toe = "djfhksfsjlskdg \"Fdsgdsf\" hfsdfh \"dshfsg\" sdfhgdf\" \"sdfhdfh\" \" dfgd\" \"\""
+#print(toe)
+#tie = "This is 'my\nstring\tsdf;ldfg  sd'''g\n\"\"\"vvldk  \" \"  sdfh' 'dsg \nstring  \""
+#print(tie)
+money = 'This is \'my\nstring\tsdf;ldfg  sd\'\'\'g\n"""vvldk  " "  sdfh\' \'dsg \nstring  "'
+print(money)
