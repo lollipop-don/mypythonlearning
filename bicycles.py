1f2db397d6ec500c28980aef52856972632e0e0c
lollipop-don <tikotyablaze@gmail.com>
2025-03-14 00:27:17 +0400

    Create bicycles.py

---
+++ bicycles.py
@@ -0,0 +1,16 @@
+bicycles = ['treck','canondale','redline','specilized']
+#print(type(bicycles))
+#print(bicycles)
+#print(bicycles[0])
+#print(bicycles[3].title())
+#print(bicycles[-1])
+#print(bicycles[-2])
+#print(f'my first bicycle was a {bicycles[0].title()}')
+#print(f'my last bicycle was a {bicycles[-1]}')
+friends = ['ana','mari','elene']
+#print(friends[0])
+#print(friends[1])
+#print(friends[2])
+print(f'hello {friends[0]}')
+print(f'hello {friends[1]}')
+print(f'hello {friends[2]}')
