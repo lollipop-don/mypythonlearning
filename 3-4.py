fc62773f5fa878d4378de88e69ee37c405a7cd5f
Tiko Tkabladze <tikotyablaze@gmail.com>
2025-03-14 23:47:08 +0400

    1:  3-4.py my first exersice from book 2: How pop and remove works for list

---
+++ 3-4.py
@@ -0,0 +1,4 @@
+guests = ['elene','ana','mari']
+print(f'{guests[0]} please come to dinner')
+print(f'{guests[1]} please come to dinner')
+print(f'{guests[2]} please come to dinner')
--- motorcycles.py
+++ motorcycles.py
@@ -17,4 +17,10 @@ motorcycles.insert(0,'ducati')
 motorcycles.insert(2,'tiko')
 del motorcycles[2]
 removed_element = motorcycles.pop()
+my_first = motorcycles.pop(0)
+
+too_expensive = 'yamaha'
+motorcycles.remove(too_expensive)
+
+print(f'{too_expensive.title()} is too expensive for me')
 pass
