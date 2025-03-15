924f033980855facf9e77add7a8e1f1ec79ba04f
Tiko Tkabladze <tikotyablaze@gmail.com>
2025-03-15 22:45:06 +0400

    3-6.py added

---
+++ 3-6.py
@@ -0,0 +1,14 @@
+guests = ['elene','ana','mari']
+print(f'{guests[0]} please come to dinner')
+print(f'{guests[1]} please come to dinner')
+print(f'{guests[2]} please come to dinner')
+print(f'{guests[2]} can not come to dinner')
+guests[2] = 'anamaria'
+print(f'{guests[0]} please come to dinner')
+print(f'{guests[1]} please come to dinner')
+print(f'{guests[2]} please come to dinner')
+print('everyone i found a bigger dinner table')
+guests.insert(0,'tekla')
+guests.insert(2,'nini')
+guests.append('barbare')
+pass
