f7354ac7b27ddb12bf05be1fbb21a8516551110e
Tiko Tkabladze <tikotyablaze@gmail.com>
2025-03-15 22:27:08 +0400

    3-5.py added

---
+++ 3-5.py
@@ -0,0 +1,9 @@
+guests = ['elene','ana','mari']
+print(f'{guests[0]} please come to dinner')
+print(f'{guests[1]} please come to dinner')
+print(f'{guests[2]} please come to dinner')
+print(f'{guests[2]} can not come to dinner')
+guests[2] = 'anamaria'
+print(f'{guests[0]} please come to dinner')
+print(f'{guests[1]} please come to dinner')
+print(f'{guests[2]} please come to dinner')
