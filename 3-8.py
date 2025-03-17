8c2e99a9b634728b6c96690d8dcae0ab040e69c5
Tiko Tkabladze <tikotyablaze@gmail.com>
2025-03-17 21:51:03 +0400

    last exersice of chapter 3

---
+++ 3-8.py
@@ -0,0 +1,17 @@
+places = ['paris','london','tbilisi','usa','japan']
+print(places)
+print(f'{sorted(places)}')
+print(places)
+print(f'{sorted(places,reverse=True)}')
+print(places)
+places.reverse()
+print(places)
+places.reverse()
+print(places)
+places.sort()
+print(places)
+places.sort(reverse=True)
+print(places)
+print(f'i have visited this many places {len(places)}')
+
+pass
--- cars.py
+++ cars.py
@@ -1,19 +1,19 @@
 cars = ['bmw','audi','toyota','subaru']
 
 
-#sorts list in ascending order
+#sorts list in ascending order(modifies list)
 cars.sort()
 
-#sorts list in descending order
+#sorts list in descending order(modifies list)
 cars.sort(reverse=True)
 
 #print list in sorted but doesnt change the list itself 
 cars = ['bmw','audi','toyota','subaru']
 print(f'here are the original cars {cars}')
-print(f'here is the sorted list {sorted(cars)}')
+print(f'here is the sorted list {sorted(cars,reverse=True)}')
 print(f'these are the cars after sorting {cars}')
 
-#how to reverse list
+#how to reverse list(and modifies list)
 cars = ['bmw','audi','toyota','subaru']
 cars.reverse()
 
