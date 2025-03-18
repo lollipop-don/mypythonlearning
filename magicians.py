08ab147f1c153d914a7a1035fdf2989f58d3e6a0
Tiko Tkabladze <tikotyablaze@gmail.com>
2025-03-18 21:56:55 +0400

    how to create a loop and repeate the same operations

---
+++ magicians.py
@@ -0,0 +1,10 @@
+magicians = ['alisa','david','karolina']
+for value in magicians:
+  print(f'{value.title()}, that was a great trick!')
+  print(f'i cant wait to see your next trick,{value.title()}!')
+  print("\n")
+
+
+
+
+pass
