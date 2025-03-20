7c7c01dea5111535c1f84669325435d50841633e
Tiko Tkabladze <tikotyablaze@gmail.com>
2025-03-20 22:28:07 +0400

    1: count from 2-11 by 2 steps 2: store the square of numbers in a list

---
+++ even_numbers.py
@@ -0,0 +1,4 @@
+#count from 2-11 by 2 steps
+even_numbers = list(range(2,11,2))
+print(even_numbers)
+
---
+++ square_numbers.py
@@ -0,0 +1,6 @@
+numbers = []
+for value in range(2,1001):
+    numbers.append(value*value)
+print(numbers)
+pass
+# [4 9 16 25 .. 400]
