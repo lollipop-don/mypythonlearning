53d3899a6ad645e5c37950fd1186c5cc10dc1b14
Tiko Tkabladze <tikotyablaze@gmail.com>
2025-03-26 20:34:41 +0400

    repeat knowledge about loop

---
+++ tiko.py
@@ -0,0 +1,18 @@
+numbers = []
+for value in range(1,101,2):
+    numbers.append(value**3)
+print(numbers)
+
+friends = ['ana','mari','tekla']
+for value in friends:
+    print(value)
+
+even_numbers = list(range(2,101,2))
+
+print(even_numbers)
+
+even_numbers = []
+for value in range(2,101,2):
+    even_numbers.append(value)
+
+print(even_numbers)    
