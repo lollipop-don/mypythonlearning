70d725dff19e6d6da124ed3501e49f5b6e6747d5
tinatin tyabladze <tikotyablaze@gmail.com>
2025-03-31 20:16:44 +0400

    1: repetition of loops 2: list comprehension

---
+++ ccc.py
@@ -0,0 +1,21 @@
+we = ['now','when', 'tt', 'rert']
+
+# we = []
+# for value in range(1,11):
+#     we.append(value)
+# print(we)     
+
+#we = list(range(1,11))
+#print(we) 
+# for value in we:
+#     print(value) 
+
+
+digits = [0,1,2,3,4,5,6,7,8,9]
+#find min element in list
+min_element = min(digits)
+
+max_element = max(digits)
+print(f'min element is {min_element} and max element is {max_element}')
+sum_element = sum(digits)
+print(sum_element)
---
+++ squares.py
@@ -0,0 +1,10 @@
+# squares = []
+# for value in range(1,11):
+#     squares.append(value**2)
+# print(squares)    
+
+
+# list comprehension
+squares = [value*3+2 for value in range(1, 11)]
+
+print(squares)
