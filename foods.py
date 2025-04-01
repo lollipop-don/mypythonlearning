7ab3f1b0a515867788593773901771b875c5a650
tinatin tyabladze <tikotyablaze@gmail.com>
2025-04-01 20:17:49 +0400

    Start using list slices

---
+++ foods.py
@@ -0,0 +1,32 @@
+my_foods = ['pizza','fallafel','carrot cake']
+
+
+
+print(f'my_foods created on address: {id(my_foods)}.')
+
+first_two_foods = my_foods[:2]
+print(f'first_two_foods is created on address: {id(first_two_foods)}.')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ players.py
@@ -0,0 +1,20 @@
+players = ['charels','martina','elene','ana','florence']
+
+
+
+# print(players[0:3])
+
+
+# middle_players = players[1:4]
+# print(middle_players)
+
+# first_four_players = players[:4]
+
+# last_three_players = players[2:]
+# print(last_three_players)
+
+
+
+for value in players[:3]:
+    print(value.title())
+    
