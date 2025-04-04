d8caf7b7e5588f6a6ea25a45f388bf963f0af52b
tinatin tyabladze <tikotyablaze@gmail.com>
2025-04-04 23:24:54 +0400

    1: learn and understand difference between if--if/else 2: >, <, >=, <=, ==, !=, and, or.

---
+++ cars_new.py
@@ -0,0 +1,8 @@
+cars = ['audi','bmw','subaru','toyota']
+for value in cars:
+    if value == 'bmw' or value == 'toyota':
+        print(value.title())
+    else:
+        print(value)
+
+
---
+++ not_equal.py
@@ -0,0 +1,3 @@
+my_car = 'bmw'
+if my_car != 'bmw':
+    print(f'this is not my car')
