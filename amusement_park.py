6406b2d29df37719348a4a6ea534ffd697539746
tinatin tyabladze <tikotyablaze@gmail.com>
2025-04-05 20:57:20 +0400

    1: check if an element is or isnt in list 2: how  if/elif/elif/else works

---
+++ amusement_park.py
@@ -0,0 +1,11 @@
+
+age = 12
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 25
+elif age < 40:
+    price = 50
+else:
+    price = 0
+print(f'your admision cost is {price}')
---
+++ banned_users.py
@@ -0,0 +1,16 @@
+banned_users = ['andrew','caroline','david']
+
+user = 'marie'
+# if user in banned_users:
+#     print(f'user {user}is banned')
+if user not in banned_users:
+    print(f'user {user} is free to use')
+
+
+
+
+
+
+t = ('23',)
+
+a = False
