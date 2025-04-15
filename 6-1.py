6328d5139eb2704b3b9e06e724c41a8ed94a7989
tinatin tyabladze <tikotyablaze@gmail.com>
2025-04-15 21:49:17 +0400

    1: 6-1.py print dictionary's each value by key 2: 6-3.py print dictionary's each key and value 3: user.py iterate each key-value in dictionary using for loop 4: new english words

---
+++ 6-1.py
@@ -0,0 +1,8 @@
+person = {
+    'first_name' : 'gigi',
+    'last_name' : 'tyablaze',
+    'age' : 9,
+    'city' : 'tskaltubo'
+}
+
+print(f"{person['first_name']} {person['last_name']} {person['age']} {person['city']}")
---
+++ 6-3.py
@@ -0,0 +1,10 @@
+words = {
+    'loop' : 'iterates elements', 
+    'if' : 'conditional select', 
+    'list' : 'stores elements', 
+    'comprehension' : 'creates list using for loop'
+}
+print(f"loop : {words['loop']}") 
+print(f"if : {words['if']}")
+print(f"list : {words['list']}")
+print(f"comprehension : {words['comprehension']}")
