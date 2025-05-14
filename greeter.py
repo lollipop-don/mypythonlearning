d31e52e76eca04113bd998a2c9443858d12e2a7c
tinatin tyabladze <tikotyablaze@gmail.com>
2025-05-14 20:44:24 +0400

    1: positional arguments keyword arguments default arguments

--- greeter.py
+++ greeter.py
@@ -1,7 +1,29 @@
-def greet_user():
-    print('hello')
-    print('nervebs mishli')
+def greet_user(username):
+    print(f'hello {username}')
 
-    
-print('hi')
-greet_user()
+
+
+
+
+greet_user('tiko')
+greet_user('gigi')
+greet_user('kate')
+
+# number = input('enter number')
+# number = int(number)
+
+def is_prime(number):
+    is_number_prime = True
+    for value in range(2,number):
+        if number % value == 0:
+            is_number_prime = False
+            break
+        
+    if is_number_prime == True:
+        print(f'the {number} is prime')
+    else:
+        print(f'the {number} is not prime')
+
+
+for value in range(2, 101):
+    is_prime(value)
--- pets.py
+++ pets.py
@@ -6,4 +6,21 @@ while 'cat' in pets:
     pets.remove('cat')
 
 
-print(pets)
+# print(pets)
+
+
+def describe_pet(pet_name, animal_type = "dog"):  #defualt argument is dog
+    print(f'i have {animal_type}')
+    print(f"my {animal_type}'s name is {pet_name}")
+
+describe_pet('willy', 'dog') 
+
+
+describe_pet('harry', 'hamster') #call function by positional arguments
+# describe_pet(animal_type = 'hamster', pet_name = 'harry') # call function by keyword arguments
+# describe_pet(pet_name = 'harry', animal_type = 'hamster')
+describe_pet('jim', 'dog') #positional arguments
+describe_pet(animal_type ='dog', pet_name = 'loma')
+
+describe_pet(pet_name= 'loki')
+describe_pet(pet_name= 'little hamster', animal_type= 'hamster')
