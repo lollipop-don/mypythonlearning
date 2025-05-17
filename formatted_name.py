0e66218c2fb4ffd3f6367e384726c2499ccd588b
tinatin tyabladze <tikotyablaze@gmail.com>
2025-05-17 21:47:53 +0400

    1: appended two functions: add_two_numbers, multiplie_three_numbers 2: i understand how return works

--- formatted_name.py
+++ formatted_name.py
@@ -2,5 +2,20 @@ def get_formatted_name(first_name, last_name):
     full_name = f'my name is {first_name.title()} and last name {last_name.title()}'
     return full_name
 
-_f_name = get_formatted_name('tiko', 'tyablaze')
-print(_f_name)
+# _f_name = get_formatted_name('tiko', 'tyablaze')
+# print(_f_name)
+
+
+def add_two_numbers(number1, number2):
+    number = number1 + number2
+    return number
+
+def multiplie_three_numbers(number1, number2, number3):
+    number = number1 * number2 * number3
+    return number
+  
+answer = multiplie_three_numbers(10,12345678,11)
+print(answer)
+
+# answer = add_two_numbers(3,5)
+# print(answer)
